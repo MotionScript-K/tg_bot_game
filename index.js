5940c5e25a1b8c4b80188ccfd1f6844a7995e96b
Kirill <125693210+MotionScript-K@users.noreply.github.com>
2023-03-18 13:02:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const TelegramBot = require('node-telegram-bot-api');
-//подключаем сервер
-const express = require('express');
-const cors = require('cors');
 
 const token = '5641035298:AAGRif1G1MXaWug4wED3j_D9KWePqwOmMlU';
 const webAppUrl = 'https://visionary-frangipane-efddf1.netlify.app';
 
 const bot = new TelegramBot(token, {polling: true});
-//для работы сервера
-const app = express();
-app.use(express.json());
-app.use(cors());
+
 
 bot.on('message', async(msg) => {
     const chatId = msg.chat.id;
@@ -30,24 +24,3 @@ bot.on('message', async(msg) => {
   };
       
 });
-
-
-//http ручка
-app.post('/web-data', async (req, res) => {
-  const {queryId, userName, otvet} = req.body;
-  try {
-    await bot.answerWebAppQuery(queryId,{
-      type: 'article',
-      id: queryId,
-      title: 'Успешное завершение',
-      input_message_content: {message_text: 'Поздравляем '+ userName + otvet}
-    })
-    return res.status(200).json({});
-  }catch(e){
-    return res.status(500).json({})
-    }
-});
-
-// запускаем сервер
-const PORT = 8000;
-app.listen(PORT, () => console.log('server start on PORT '+ PORT));
