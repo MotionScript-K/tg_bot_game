c3e6d42a5762c290c11f186f0f1dec01d4968343
Kirill <125693210+MotionScript-K@users.noreply.github.com>
2023-03-24 14:48:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,26 +6,43 @@ const webAppUrl = 'https://visionary-frangipane-efddf1.netlify.app';
 const bot = new TelegramBot(token, {polling: true});
 
 
-bot.on('message', async(msg) => {
-    const chatId = msg.chat.id;
-    const text = msg.text;
+const keyboardTwo = [
+  [{
+    text: 'Поехали !',
+    web_app: { url: webAppUrl }
+  }]
+]
 
-  if(text === '/start'){
-    //если написал Старт - создание кнопки inline
-    await bot.sendMessage(chatId,'Испытай удачу', {
-        reply_markup: {
-            inline_keyboard:[
-                [{text: 'найди сертификат', web_app: {url: webAppUrl}}]
-            ]
-        }
-    });
-    }else{bot.sendMessage(chatId,'Испытай удачу', {
-        reply_markup: {
-            inline_keyboard:[
-                [{text: 'найди сертификат', web_app: {url: webAppUrl}}]
-            ]
-        }
-    });
-   };
+bot.onText(/\/start/, async (msg, [source, match]) => {
+  const chatId = msg.chat.id;
+  const firstName = msg.from.first_name;
+  const html = `
+<strong>${firstName}</strong>
 
-});
+<i>Описание правил игры\nкак ей пользоваться...\nтут может быть текстовой блок ...</i>
+
+<i>Игра доступна сегодня\n01.01.2023\nколичство попыток (ограничено)</i>
+
+`
+  await bot.sendMessage(chatId, html, {
+    parse_mode: 'HTML'
+  });
+  await bot.sendSticker(chatId, './ref.webp')
+  await bot.sendMessage(chatId, 'Желаем удачи в конкурсе и выйграть приз !', {
+    reply_markup: {
+      inline_keyboard: keyboardTwo
+    }
+  });
+})
+
+//ответ на кнопку в приложении
+bot.on('message', async (msg) => {
+  const chatId = msg.chat.id;
+  if (msg?.web_app_data?.data) {
+    try {
+      await bot.sendMessage(chatId, 'Обратная связь получена');
+      const data = JSON.parse(msg?.web_app_data?.data)
+      await bot.sendMessage(chatId, data);
+    } catch (e) { }
+  }
+})
