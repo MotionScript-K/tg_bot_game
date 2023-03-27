329e8cf286015680399c70b15b2b4f1fb12b17b5
Kirill <125693210+MotionScript-K@users.noreply.github.com>
2023-03-27 13:32:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const TelegramBot = require('node-telegram-bot-api');
-require('dotenv').config();
-const token = process.env.TOKEN;
+//require('dotenv').config();
+//const token = process.env.TOKEN;
+const token = '5641035298:AAGRif1G1MXaWug4wED3j_D9KWePqwOmMlU';
 
 const webAppUrl = 'https://visionary-frangipane-efddf1.netlify.app';
 
-const express = require('express');
-const cors = require('cors');
+// const express = require('express');
+// const cors = require('cors');
 
 const bot = new TelegramBot(token, {polling: true});
 
-const app = express();
-app.use(express.json());
-//app.use(cors());
+// const app = express();
+// app.use(express.json());
+// app.use(cors());
 
 const keyboardTwo = [
   [{
-    text: 'Поехали !',
+    text: 'Начать игру',
     web_app: { url: webAppUrl }
   }]
 ]
@@ -34,48 +35,49 @@ bot.onText(/\/start/, async (msg, [source, match]) => {
   await bot.sendMessage(chatId, html, {
     parse_mode: 'HTML'
   });
-  await bot.sendSticker(chatId, './ref.webp')
-  await bot.sendMessage(chatId, 'Желаем удачи в конкурсе и выйграть приз !', {
+  await bot.sendSticker(chatId, './ref.webp', {
     reply_markup: {
-      inline_keyboard: keyboardTwo
+      resize_keyboard: true,
+      keyboard: keyboardTwo
     }
-  });
+  })
 })
 
 //ответ на кнопку в приложении
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
   if (msg?.web_app_data?.data) {
+    console.log(msg);
     try {
-      await bot.sendMessage(chatId, 'Обратная связь получена');
-      const data = JSON.parse(msg?.web_app_data?.data)
-      await bot.sendMessage(chatId, data);
-    } catch (e) { }
+      await bot.sendMessage(chatId, `${msg.from.first_name}, сейчас мы ${msg.web_app_data.data}`);
+    } catch (e) {
+      await bot.sendMessage(chatId, 'что то пошло не так');
+    }
   }
 })
 
-app.post('/', async (req,res)=>{
-  console.log(req.query);
-  const {queryId, otvet} = req.body;
-try{
-  await bot.answerWebAppQuery(queryId, {
-    type: 'article',
-    id: queryId,
-    title: 'Успешно',
-    input_message_content: {message_text: 'текст пользователю !!! '+ otvet}
-  })
-  return res.status(200).json('Сервер работает');
-} catch(e) {
-  await bot.answerWebAppQuery(queryId, {
-    type: 'article',
-    id: queryId,
-    title: 'что то пошло не так',
-    input_message_content: {message_text: 'что то пошло не так 2'}
-})
-return res.status(500).json('что то пошло не так');
-}
+// app.post('/web-data', async (req,res)=>{
+//   const {queryId, otvet} = req.body;
+// try{
+//   await bot.answerWebAppQuery(queryId, {
+//     type: 'article',
+//     id: queryId,
+//     title: 'Успешно',
+//     input_message_content: {message_text: 'текст пользователю !!! '+ otvet}
+//   })
+//   return res.status(200).json({});
+// } catch(e) {
+//   await bot.answerWebAppQuery(queryId, {
+//     type: 'article',
+//     id: queryId,
+//     title: 'что то пошло не так',
+//     input_message_content: {message_text: 'что то пошло не так 2'}
+// })
+// return res.status(500).json({});
+// }
 
-})
+// })
 
-const PORT = 5000;
-app.listen(PORT, ()=> console.log('server Start '+ PORT));
+// const PORT = 8000;
+// app.listen(PORT, ()=> console.log('server Start '+ PORT));
+
